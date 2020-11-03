423d942ddcc54d86849b9a907f8e6d205ec7fc40
web5 <web5@ayla.dt>
2020-11-03 13:55:52 +0100

    mas estilo

--- themes/reservadepistasdeportivas/header.php
+++ themes/reservadepistasdeportivas/header.php
@@ -48,16 +48,14 @@
                     <?php endif; ?>
 
                 </div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                
 
                 <?php
                 wp_nav_menu(array(
                 'theme_location'    => 'primary',
                 'container'       => 'div',
                 'container_id'    => 'main-nav',
-                'container_class' => 'collapse navbar-collapse justify-content-end',
+                'container_class' => ' justify-content-end',
                 'menu_id'         => false,
                 'menu_class'      => 'navbar-nav',
                 'depth'           => 3,
